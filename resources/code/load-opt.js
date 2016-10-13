9ffda9931f7d79b47e3d427c81e9b6027c449e63
Franziska Hinkelmann <franzih@chromium.org>
2016-10-13 10:46:10 +0200

    Change code

--- resources/code/load-opt.js
+++ resources/code/load-opt.js
@@ -6,10 +6,10 @@ function load(obj) {
 
 load({x:5});
 load({x:17});
-load({x:17, a: 1});
-load({x:17, b: 1});
-load({x:17, c: 1});
+load({x:3, a: 1});
+load({x:7, b: 1});
+load({x:42, c: 1});
 %OptimizeFunctionOnNextCall(load);
-load({x:42});
+load({x:13});
 
 
